a3bac07cb3b80d667a744bdfcfc8f500230532e4
marcel0alves <marcelojudoka@gmail.com>
2020-08-01 12:33:43 -0400

    Updated some scripts.

--- .scripts/temperature.sh
+++ .scripts/temperature.sh
@@ -2,5 +2,5 @@
 
 cputemp=$(sensors | awk '/Core/ {print substr ($3, 2, 2) " C"}' | tr '\n' ' ')
 
-dunstify "CPU Temp" "${cputemp}"
-#notify-send -u low "CPU Temp" "${cputemp}"
+#dunstify "CPU Temp" "${cputemp}"
+notify-send -u low "CPU Temp" "${cputemp}"
